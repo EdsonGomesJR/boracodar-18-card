45e86cc2dc25eaa5d8ed51c6d6d835fe5a9cbb17
EdsonGomesJr <edyz86@hotmail.com>
2023-05-06 12:22:56 -0300

    adjust

--- src/components/Slider2/Slider2.tsx
+++ src/components/Slider2/Slider2.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState , ReactNode} from 'react'
 import {TiChevronLeftOutline, TiChevronRightOutline} from 'react-icons/ti'
 import cx from 'classnames';
 const CARDS = 10;
@@ -7,7 +7,11 @@ const MAX_VISIBILITY = 3;
 const classes = cx (
   'flex bg-red-500'
 )
-const Card = ({title , content} ) => (
+interface ICard {
+  title: string;
+  content: string;
+}
+const Card = ({title , content} : ICard) => (
   
   <div className={`card `}>
     <h2>{title}</h2>
@@ -15,7 +19,11 @@ const Card = ({title , content} ) => (
   </div>
 );
 
-const Carousel = ({children}) => {
+interface ICarouselProps {
+  children: ReactNode
+}
+
+const Carousel = ({children} : ICarouselProps) => {
   const [active, setActive] = useState(2);
   const count = React.Children.count(children);
   
@@ -24,7 +32,7 @@ const Carousel = ({children}) => {
       {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><TiChevronLeftOutline/></button>}
       {React.Children.map(children, (child, i) => (
         <div className={`card-container  `}  style={{
-            '--active': i === active ? 1 : 0.5,
+            '--active': i === active ? 1 : 0,
             '--offset': (active - i) / 3,
             '--direction': Math.sign(active - i),
             '--abs-offset': Math.abs(active - i) / 3,
@@ -47,6 +55,10 @@ export default function Slider2() {
       <Card  key={i} title={'Card ' + (i + 1)} content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
     ))}
   </Carousel>
+  <div>
+    <h2>Oiii</h2>
+    <p>genteee</p>
+  </div>
   </div>
   )
 }
