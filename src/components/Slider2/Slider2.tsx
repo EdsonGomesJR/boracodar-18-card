5d7d39b7b154a746b5aa6acd0e00d4657e717310
EdsonGomesJr <edyz86@hotmail.com>
2023-05-06 10:43:02 -0300

    first

--- src/components/Slider2/Slider2.tsx
+++ src/components/Slider2/Slider2.tsx
@@ -20,18 +20,18 @@ const Carousel = ({children}) => {
   const count = React.Children.count(children);
   
   return (
-    <div className='carousel bg-red-200'>
+    <div className='carousel '>
       {active > 0 && <button className='nav left' onClick={() => setActive(i => i - 1)}><TiChevronLeftOutline/></button>}
       {React.Children.map(children, (child, i) => (
-        <div className={`card-container ${i === active ? 'opacity-1' : 'opacity-0'}`}  style={{
-            '--active': i === active ? 1 : 0,
+        <div className={`card-container  `}  style={{
+            '--active': i === active ? 1 : 0.5,
             '--offset': (active - i) / 3,
             '--direction': Math.sign(active - i),
             '--abs-offset': Math.abs(active - i) / 3,
             'pointerEvents': active === i ? 'auto' : 'none',
             'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
             'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-          }}>
+          } as React.CSSProperties}>
           {child}
         </div>
       ))}
@@ -46,6 +46,7 @@ export default function Slider2() {
     {[...new Array(CARDS)].map((_, i) => (
       <Card  key={i} title={'Card ' + (i + 1)} content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
     ))}
-  </Carousel></div>
+  </Carousel>
+  </div>
   )
 }
